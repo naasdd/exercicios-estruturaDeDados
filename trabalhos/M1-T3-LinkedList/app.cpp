11cfda71b94da97e006bf3ec26ad556de97f3768
naasdd <josegabrielg2014@gmail.com>
2026-03-13 15:26:42 -0300

    Finalizado M1-T3-Listas_Simples

--- trabalhos/M1-T3-LinkedList/app.cpp
+++ trabalhos/M1-T3-LinkedList/app.cpp
@@ -5,26 +5,45 @@ using namespace std;
 
 int main()
 {
-    cout << "Criando lista simples: " << endl;
+    cout << "\nCriando lista simples: " << endl;
     ListaSimples *lista = criarListaSimples();
     adicionarInicioDaListaSimples(lista, 30);
     adicionarInicioDaListaSimples(lista, 6);
     adicionarInicioDaListaSimples(lista, 89);
 
-    cout << "Adicionando 69420 ao final da lista" << endl;
+    cout << "\nAdicionando 69420 ao final da lista" << endl;
     adicionarUltimoDaLista(lista, 69420);
 
-    cout << " Imprimindo lista simples " << endl;
+    cout << "\nImprimindo lista simples " << endl;
     imprimirListaSimples(lista);
 
-    cout << "Removendo o ultimo da lista" << endl;
+    cout << "\nRemovendo o ultimo da lista" << endl;
     removerUltimoDaLista(lista);
 
-    cout << "Imprimindo lista simples " << endl;
+    cout << "\nImprimindo lista simples " << endl;
     imprimirListaSimples(lista);
 
+    cout << "\nInserindo ordenado 50, 10, 30." << endl;
+    inserirOrdenado(lista, 50);
+    inserirOrdenado(lista, 10);
+    inserirOrdenado(lista, 30);
+
+    contarNos(lista);
  
-    
+    cout << "\nImprimindo lista simples " << endl;
+    imprimirListaSimples(lista);
+
+    cout << "\nInvertendo Lista" << endl;
+    inverterLista(lista);
+
+    cout << "\nImprimindo lista simples " << endl;
+    imprimirListaSimples(lista);
+
+    cout << "\nLiberando Lista" << endl;
+    liberarLista(lista);
+
+    cout << "\nImprimindo lista simples " << endl;
+    imprimirListaSimples(lista);
 
     return 0;
 }
--- trabalhos/M1-T3-LinkedList/lista_simples.cpp
+++ trabalhos/M1-T3-LinkedList/lista_simples.cpp
@@ -2,10 +2,11 @@
 #include <cstdlib>
 #include "lista_simples.h"
 
-using namespace  std;
+using namespace std;
 
-No *_criarNo(int valor){
-    No *no = (No*)calloc(1, sizeof(No));
+No *_criarNo(int valor)
+{
+    No *no = (No *)calloc(1, sizeof(No));
 
     no->valor = valor;
     no->proximo = NULL;
@@ -13,69 +14,116 @@ No *_criarNo(int valor){
     return no;
 }
 
-ListaSimples *criarListaSimples(){
-    ListaSimples *lista = (ListaSimples*)calloc(1, sizeof(ListaSimples));
+ListaSimples *criarListaSimples()
+{
+    ListaSimples *lista = (ListaSimples *)calloc(1, sizeof(ListaSimples));
     lista->inicio = NULL;
 
     return lista;
 }
 
-void adicionarInicioDaListaSimples(ListaSimples *listaSimples, int valor){
+void adicionarInicioDaListaSimples(ListaSimples *listaSimples, int valor)
+{
     No *no = _criarNo(valor);
 
     no->proximo = listaSimples->inicio;
     listaSimples->inicio = no;
 }
 
-void imprimirListaSimples(ListaSimples *listaSimples){
+void imprimirListaSimples(ListaSimples *listaSimples)
+{
     No *no = listaSimples->inicio;
 
-    while(no != NULL){
-        cout<< no->valor <<" ";
+
+    while (no != NULL)
+    {
+        cout << no->valor << " ";
         no = no->proximo;
     }
-    cout<<endl;
+    cout << endl;
 }
 
-void removerUltimoDaLista(ListaSimples *listaSimples){
+void removerUltimoDaLista(ListaSimples *listaSimples)
+{
     No *atual = listaSimples->inicio;
-    
+
     while (atual->proximo->proximo != NULL)
     {
-        atual = atual->proximo;    
+        atual = atual->proximo;
     }
-    No *temp = atual;
+    No *temp = atual->proximo;
     atual->proximo = NULL;
     free(temp);
-    
 }
 
-void adicionarUltimoDaLista(ListaSimples *listaSimples, int valor){
+void adicionarUltimoDaLista(ListaSimples *listaSimples, int valor)
+{
     No *no = _criarNo(valor);
 
     No *atual = listaSimples->inicio;
-    while(atual->proximo != NULL){
+    while (atual->proximo != NULL)
+    {
         atual = atual->proximo;
     }
     atual->proximo = no;
 }
 
-void inserirOrdenado(ListaSimples *listaSimples, int valor){
+void inserirOrdenado(ListaSimples *listaSimples, int valor)
+{
     No *novoNo = _criarNo(valor);
 
     No *atual = listaSimples->inicio;
 
-    while(atual->valor > valor && (atual->proximo != NULL && atual->proximo->valor < valor)){
+    while (atual->valor < valor && (atual->proximo != NULL && atual->proximo->valor < valor))
+    {
         atual = atual->proximo;
     }
 
-
     novoNo->proximo = atual->proximo;
     atual->proximo = novoNo;
-
-
-
 }
 
+void contarNos(ListaSimples *listaSimples)
+{
+    No *atual = listaSimples->inicio;
+    int soma = 0;
 
+    while (atual != NULL)
+    {
+        atual = atual->proximo;
+        soma++;
+    }
 
+    cout << "A quantidade de nos eh de: " << soma << endl;
+}
+
+void inverterLista(ListaSimples *listaSimples)
+{
+    No *atual = listaSimples->inicio;
+    No *listaInvertida = NULL;
+
+    while (atual != NULL)
+    {
+        No *temp = atual;
+        No *temp2 = listaInvertida;
+        No *proximoNo = atual->proximo;
+        listaInvertida = temp;
+        listaInvertida->proximo = temp2;
+        atual = proximoNo;
+    }
+
+    listaSimples->inicio = listaInvertida;
+}
+
+void liberarLista(ListaSimples *listaSimples)
+{
+    No *atual = listaSimples->inicio;
+
+    while (atual != NULL)
+    {
+        No *temp = atual;
+        atual = atual->proximo;
+        free(temp);
+    }
+    listaSimples->inicio = NULL;
+}
